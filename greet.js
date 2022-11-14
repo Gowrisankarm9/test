dd35e8eb19f9ac8928616bb9498a1a5a82d456d8
Gowrisankarm9 <gowrisankarm9@gmail.com>
2022-11-14 13:16:13 +0530

    Tsssss added

--- greet.js
+++ greet.js
@@ -1,6 +1,7 @@
 var a = [1, 2];
 console.log(a);
-console.log("testing sample1");
+// console.log("testing sample1");
+console.log("TSssss");
 console.log("testing trial");
 console.log("testing sample2");
 // console.log('TS');
