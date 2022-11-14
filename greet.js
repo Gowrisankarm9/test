5f062602eaf4063fe81f4da5ae33be860ce09aa8
Gowrisankarm9 <gowrisankarm9@gmail.com>
2022-11-14 12:59:21 +0530

    ts commented

--- greet.js
+++ greet.js
@@ -1,6 +1,7 @@
 var a = [1, 2];
 console.log(a);
-console.log("testing sample1");
+// console.log("testing sample1");
+console.log('TS');
 
 console.log("testing sample3");
 console.log("testing sample4");
