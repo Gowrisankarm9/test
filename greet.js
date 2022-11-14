2eab0415c07fcbbf3987692e6c6912dfe379c1cf
Gowrisankarm9 <gowrisankarm9@gmail.com>
2022-11-14 13:14:40 +0530

    same greet

--- greet.js
+++ greet.js
@@ -1,9 +1,11 @@
 var a = [1, 2];
 console.log(a);
 console.log("testing sample1");
+console.log("testing trial");
+console.log("testing sample2");
 // console.log('TS');
 
 console.log("testing sample3");
 console.log("testing sample4");
 
-
+console.log("testing trial1");
